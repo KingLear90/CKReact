2ec81c66699f8a672d85df03f171c139e294dac7
Alan <lic.sanchezalan@gmail.com>
2024-09-25 00:15:39 -0300

    primeras componetizaciones

---
+++ CKReact/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+            <header className="encabezado">
+                <div>ChessKindom</div>
+                <div>Home</div>
+                <div>Aprendizaje</div>
+                <div>Galería</div>
+                <div>Contacto</div>
+            </header>
+    );
+}
+
