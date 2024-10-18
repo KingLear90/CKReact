a6c4d22be4fd71700a863d32c31549bf579673e2
Alan <lic.sanchezalan@gmail.com>
2024-10-18 13:55:19 -0300

    deleted non-react files

--- JS/index.js
+++
@@ -1,9 +0,0 @@
-let live = document.getElementById("live")
-
-window.onscroll = () => {
-    if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
-        live.style.display= "block";
-    } else {
-        live.style.display= "none";
-    }
-}
